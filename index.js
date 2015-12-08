be161d96e336cbbd320262acea17981e45b75cf7
nsanpedro <nicolassanpedro@gmail.com>
2015-12-08 17:30:33 -0300

    2nd Commit

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
 //js de mongo+node - prueba
 
+var MongoClient = require('mongodb').MongoClient;
+
+var url = 'mongodb://localhost:27017/myproject';
+
+MongoClient.connect(url, function (err, db){
+	if (err) {
+		process.exit(1);
+	} else {
+		console.log("Connected correctly to server");
+
+	}
+	db.close();
+});
+
+// Insert de documentos
+
+var insertDocuments = function(db, callback){
+	var collection = db.collection('documents');
+
+	collection.insert([
+	{a : 1}, {a : 2}, {a : 3}], 
+	function(err, result) {
+			console.log("Inserted 3 documents into the document collection");
+			callback(result);
+		});
+}
+
